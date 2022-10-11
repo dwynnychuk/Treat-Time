e2739f9f353c09a146d55641971dec45be8843c3
dwynnychuk <dwynnychuk@gmail.com>
2022-10-10 16:53:56 -0900

    update

--- dispense.py
+++ dispense.py
@@ -1,5 +1,22 @@
 # Dispense treat by moving servo
 
 import RPi.GPIO as GPIO
+import time
 
-print("Testing for Github")
+
+LED = 18
+# set mode for pin numbering
+# testing with LED
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(LED, GPIO.OUT)
+
+for i in range(30):
+    GPIO.output(LED, GPIO.HIGH)
+    time.sleep(0.05)
+    GPIO.output(LED, GPIO.LOW)
+    time.sleep(0.2)
+
+
+# End of Script Cleanup
+GPIO.cleanup()
