fb691132f90c3d6d8e1362a946a740052ca28b49
dwynnychuk <dwynnychuk@gmail.com>
2022-10-15 16:40:34 -0900

    establish recording for captrue

--- capture.py
+++ capture.py
@@ -1,4 +1,15 @@
 #access webcam and snap a photo 
 import cv2
 
-print('hello world')
+# initiate camera
+cam = cv2.VideoCapture(0)
+
+while True:
+    ret, image = cam.read()
+    cv2.imshow('Test',image)
+    k = cv2.waitKey(1)
+    if k != -1:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
