1622ccd215d81865f25a090dc93386d1b686b030
dwynnychuk <dwynnychuk@gmail.com>
2022-10-15 17:03:12 -0900

    functional display of image

--- capture.py
+++ capture.py
@@ -1,15 +1,17 @@
 #access webcam and snap a photo 
 import cv2
+from datetime import datetime
 
 # initiate camera
 cam = cv2.VideoCapture(0)
 
-while True:
-    ret, image = cam.read()
-    cv2.imshow('Test',image)
-    k = cv2.waitKey(1)
-    if k != -1:
-        break
+# capture and save image
+ret, image = cam.read()
+cv2.imshow('Test',image)
+cv2.waitKey(1000)
+#    k = cv2.waitKey(1)
+#    if k != -1:
+#        break
 
 cam.release()
 cv2.destroyAllWindows()
