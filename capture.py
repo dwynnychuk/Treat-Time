d6a2e0addcb7081c2794b792b25eb95bec269539
dwynnychuk <dwynnychuk@gmail.com>
2022-10-18 17:16:28 -0900

    clean up capture

--- capture.py
+++ capture.py
@@ -27,29 +27,3 @@ class Capture:
         cv2.waitKey(dispTime*1000)
         cv2.destroyAllWindows()
 
-#access webcam and snap a photo 
-
-
-# # initiate directory for saving files
-# currentDirectory = os.getcwd()
-# saveDir = currentDirectory + "/images/"
-# print(saveDir)
-
-# # initiate camera
-# cam = cv2.VideoCapture(0)
-
-# # capture and save image
-# ret, image = cam.read()
-# timeNow = str(datetime.now())
-# imDir = saveDir + timeNow + ".jpg"
-
-# time.sleep(1)
-# cv2.imwrite(imDir, image)
-
-# cv2.imread(imDir)
-# img = cv2.imread(imDir)
-# cv2.imshow("image",img)
-# cv2.waitKey(4000)
-
-# cam.release()
-# cv2.destroyAllWindows()
