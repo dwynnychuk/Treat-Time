9cf17088aa2e31a76ab8c6a08b56ccc25ce4495d
dwynnychuk <dwynnychuk@gmail.com>
2022-10-22 12:45:05 -0900

    Read, LED, servo, and image are all working

--- dispense.py
+++ dispense.py
@@ -4,32 +4,25 @@ import RPi.GPIO as GPIO
 import time
 
 
-def openDispenser(servoPin, servoFreq, servoStart, travelTime):    
+def Dispenser(servoPin, servoFreq, servoStart, servoStop, travelTime):    
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(servoPin, GPIO.OUT)
 
     servoOutput = GPIO.PWM(servoPin,servoFreq)
-    servoOutput.start(servoStart)
-    print("open")
+    servoOutput.start(0)
     servoOutput.ChangeDutyCycle(servoStart)
     time.sleep(travelTime)
-    servoOutput.stop()
-    
-
-def closeDispenser(servoPin, servoFreq, servoStop, travelTime):
-    servoOutput = GPIO.PWM(servoPin,servoFreq)
-    servoOutput.start(0)
-    print("close")
     servoOutput.ChangeDutyCycle(servoStop)
     time.sleep(travelTime)
     servoOutput.stop()
 
 
-def blink(ledPin, blinkTime):
+def blink(ledPin, blinkOnTime, blinkOffTime):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(ledPin, GPIO.OUT)
     GPIO.output(ledPin, GPIO.LOW)
     GPIO.output(ledPin, GPIO.HIGH)
-    time.sleep(blinkTime)
+    time.sleep(blinkOnTime)
     GPIO.output(ledPin, GPIO.LOW)
+    time.sleep(blinkOffTime)
     
--- main.py
+++ main.py
@@ -5,36 +5,38 @@ import EmailRead
 import time
 from capture import Capture
 
+# # # SERVO INFORMATION
+# Servo frequency is 50Hz
+# 0DEGREE is DS of 2
+# 180DEGREE is DS of 12
 
 # # # INITIALIZATIONS
 servoPin = 32
 servoFreq = 50
-servoStart = 0
+servoStart = 2
 servoStop = 12
 ledPin = 18
 travelTime = 1
 dispTime = 5
-blinkTime = 0.1
+blinkOnTime = 0.1
+blinkOffTime = 0.05
 
 # test loop for dispensing based on number
 numInitial = EmailRead.readEmails()
-
 numNow = numInitial
 
-#while numNow <= numInitial:
-#    numNow = EmailRead.readEmails()
-#    print(numNow)
+while numNow <= numInitial:
+    numNow = EmailRead.readEmails()
+    print(numNow)
 
 
 print("\n\nNew Email Alert!!!\n\n")
-#for i in range(10):
-#    dispense.blink(ledPin, blinkTime)
-#    time.sleep(blinkTime)
+for i in range(10):
+    dispense.blink(ledPin, blinkOnTime, blinkOffTime)
 
-dispense.openDispenser(servoPin, servoFreq, servoStart, travelTime)
-dispense.closeDispenser(servoPin, servoFreq, servoStart, travelTime)
-p = Capture()
-l = Capture.display(p, dispTime)
+dispense.Dispenser(servoPin, servoFreq, servoStart, servoStop, travelTime)
+treatIm = Capture()
+Capture.display(treatIm, dispTime)
 
 # # # end of script cleanup
 GPIO.cleanup()
