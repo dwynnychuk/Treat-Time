8fb8e37e12720e4e594308b4eebc6f723f473032
dwynnychuk <dwynnychuk@gmail.com>
2022-10-22 10:35:24 -0900

    able to detect new email

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import RPi.GPIO as GPIO
 import dispense
 import EmailRead
+import time
 from capture import Capture
 
 
@@ -10,10 +11,34 @@ servoPin = 32
 servoFreq = 50
 servoStart = 0
 servoStop = 12
+ledPin = 18
 travelTime = 1
 
-num = EmailRead.readEmails()
-print(num)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(ledPin, GPIO.OUT)
+
+# test loop for dispensing based on number
+numInitial = EmailRead.readEmails()
+
+numNow = numInitial
+
+while numNow <= numInitial:
+    numNow = EmailRead.readEmails()
+    GPIO.output(ledPin, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(ledPin, GPIO.LOW)
+    print(numNow)
+
+
+print("New Email Alert")
+for i in range(50):
+    GPIO.output(ledPin, GPIO.HIGH)
+    time.sleep(0.2)
+    GPIO.output(ledPin, GPIO.LOW)
+    time.sleep(0.2)
+
+
+
 
 #dispense.openDispenser(servoPin, servoFreq, servoStart, travelTime)
 #dispense.closeDispenser(servoPin, servoFreq, servoStop, travelTime)
@@ -24,6 +49,6 @@ print(num)
 
 
 # # # end of script cleanup
-#GPIO.cleanup()
+GPIO.cleanup()
 
 
